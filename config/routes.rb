e7f040aee8f309bde3f810681776e2cd24e7adac
jiaen zhang <jiaen.zhang98@gmail.com>
2021-11-23 13:56:16 +0800

    done everything except design

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #READ - all
+  root to: 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+
+  #CREATE
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create', as: :create
+
+  #READ - show
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  #UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  #DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
